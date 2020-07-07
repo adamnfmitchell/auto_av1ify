bc8167980206aecde19518cdbf56f18e959a3dc4
Adam Woods <adam@microwaved.network>
2020-07-07 21:37:32 +1000

    Updated more flexible watcher script

--- watcher.sh
+++ watcher.sh
@@ -1,37 +1,16 @@
-#!/bin/sh
-cd /config/encode/
-mkdir 300 400 600 1000 1500 2000 2250 2500
-cd /
-MONITORDIR1="/config/encode/300/"
-MONITORDIR2="/config/encode/400/"
-MONITORDIR3="/config/encode/600/"
-MONITORDIR4="/config/encode/1000/"
+#!/bin/bash
+root_dir="/"
+watch_dir="watch/"
+script_dir="config/"
+script_file="queue_encode.sh"
+escaped_root=$(echo ${root_dir} | sed 's/\//\\\//g')
 
-three() {
-inotifywait -m -r -e close_write --format "%f" "$1" | while read NEWFILE
-do
-        cp "./encode/300/${NEWFILE}" ./ && /config/raviencode.sh "${NEWFILE}" 300
-done
+watch() {
+  inotifywait -m -r -e close_write --format "%w%f" "$1" | while read new_file_path
+  do
+    bitrate=$(echo "${new_file_path}" | sed "s/${escaped_root}//g" | sed "s/\// /g" | awk '{print $2}')
+    file=$(basename "${new_file_path}")
+    eval "${root_dir}${script_dir}${script_file} ${bitrate} \"${file}\""
+  done
 }
-four() {
-inotifywait -m -r -e close_write --format "%f" "$1" | while read NEWFILE
-do
-        cp "./encode/400/${NEWFILE}" ./ && /config/raviencode.sh "${NEWFILE}" 400
-done
-}
-six() {
-inotifywait -m -r -e close_write --format "%f" "$1" | while read NEWFILE
-do
-        cp "./encode/600/${NEWFILE}" ./ && /config/raviencode.sh "${NEWFILE}" 600
-done
-}
-onek() {
-inotifywait -m -r -e close_write --format "%f" "$1" | while read NEWFILE
-do
-        cp "./encode/1000/${NEWFILE}" ./ && /config/raviencode.sh "${NEWFILE}" 1000
-done
-}
-three "$MONITORDIR1" &
-four "$MONITORDIR2" &
-six "$MONITORDIR3" &
-onek "$MONITORDIR4"
+watch "${root_dir}${watch_dir}" &
